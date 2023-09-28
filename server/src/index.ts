bb2d6f3a5393ef7e8954f7acac6f8fd3bad43882
hwangdeokthien <huynhducthien41906@gmail.com>
2023-09-28 14:47:33 +0700

    [ADD] add new session and constants

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,49 @@
 require("dotenv").config();
 import "reflect-metadata";
 import cors from "cors";
+import mongoose from "mongoose";
 import express, { json } from "express";
+import MongoStore from "connect-mongo";
+import session from "express-session";
 import { HelloResolver } from "./resolvers/hello";
 import { postgresDataSource } from "./appDataSource";
 import { ApolloServer } from "@apollo/server";
 import { buildSchema } from "type-graphql";
 import { expressMiddleware } from "@apollo/server/express4";
 import { UserResolver } from "./resolvers/user";
-
-postgresDataSource
-  .initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err);
-  });
+import { COOKIE_NAME, __prod__ } from "./constants";
 
 const main = async () => {
+  postgresDataSource
+    .initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+      console.error("Error during Data Source initialization", err);
+    });
+
   const app = express();
+
+  // session/cookie store
+  const mongoUrl = process.env.MONGODB_URL || "";
+  await mongoose.connect(mongoUrl);
+  console.log("MongoDB connected!");
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      secret: process.env.SESSION_SECRET_DEV_PROD as string,
+      store: MongoStore.create({ mongoUrl }),
+      cookie: {
+        maxAge: 1000 * 60 * 60, // ms
+        httpOnly: true, //js front end can not access the cookies
+        secure: __prod__, //cookies only work in https
+        sameSite: "lax", //protection against CSRF
+      },
+      saveUninitialized: false, // do not save empty session, from the start
+      resave: false,
+    })
+  );
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
