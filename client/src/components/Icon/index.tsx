6f12c6353ddc656c745a0c97000d76cb9116aba7
LuniuLun <nguyenducvan260903@gmail.com>
2024-01-20 15:33:47 +0700

    [UPDATE] update profile

--- client/src/components/Icon/index.tsx
+++ client/src/components/Icon/index.tsx
@@ -47,6 +47,23 @@ export const HomeIcon = ({ className }: Icon) => {
     );
 };
 
+export const BigPlusIcon = ({ className }: Icon) => {
+    return (
+        <svg
+            aria-label="Biểu tượng dấu cộng"
+            className={className}
+            fill="currentColor"
+            height="44"
+            role="img"
+            viewBox="0 0 24 24"
+            width="44"
+        >
+            <title>Biểu tượng dấu cộng</title>
+            <path d="M21 11.3h-8.2V3c0-.4-.3-.8-.8-.8s-.8.4-.8.8v8.2H3c-.4 0-.8.3-.8.8s.3.8.8.8h8.2V21c0 .4.3.8.8.8s.8-.3.8-.8v-8.2H21c.4 0 .8-.3.8-.8s-.4-.7-.8-.7z"></path>
+        </svg>
+    );
+};
+
 export const FindIcon = ({ className }: Icon) => {
     return (
         <svg
--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Footer from "../../components/Layouts/DefautLayout/Footer";
 import styles from "./profile.module.scss";
 import classNames from "classnames/bind";
+import { faBookmark, faClipboardUser, faGear, faPlus, faTableCells } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { BigPlusIcon } from "../../components/Icon";
 
 const cx = classNames.bind(styles);
 
@@ -8,10 +12,71 @@ function Profile() {
     return (
         <div className={cx("wrapper")}>
             <div className={cx("heading")}>
-                
+                <img src="/img/VanLun.jpg" alt="" className={cx("avatar-user")} />
+                <div className={cx("individual-information")}>
+                    <div className={cx("top-items")}>
+                        <div className={cx("username")}>ducvan_09</div>
+                        <button className={cx("eidt-information")}>Chỉnh sửa trang cá nhân</button>
+                        <button className={cx("show-store")}>Xem kho lưu trữ</button>
+                        <FontAwesomeIcon icon={faGear} className={cx("icon-setting")} />
+                    </div>
+                    <div className={cx("bottom-items")}>
+                        <div className={cx("interaction")}>
+                            <div className={cx("count-post")}>2 bài viết</div>
+                            <div className={cx("count-folowwer")}>223 người theo dõi</div>
+                            <div className={cx("count-folowwing")}>Đang theo dõi 1.266 người dùng</div>
+                        </div>
+                        <div className={cx("name")}>Đức Vấn</div>
+                        <div className={cx("second-name")}>LuniuLun</div>
+                        <a className={cx("link-face")}>
+                            <FontAwesomeIcon icon={faFacebook} className={cx("icon-facebook")} />
+                            Trang cá nhân Facebook
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div className={cx("content")}></div>
-            <Footer/>
+            <div className={cx("body")}>
+                <div className={cx("list-stories")}>
+                    <div className={cx("story")}>
+                        <div className={cx("wrapper-story")}>
+                            <img src="/img/Cat.jpg" alt="" srcSet="" className={cx("story")} />
+                        </div>
+                        <p>MeoMeoMeo</p>
+                    </div>
+                    <div className={cx("story")}>
+                        <div className={cx("wrapper-story")}>
+                            <img src="/img/Cat.jpg" alt="" srcSet="" className={cx("story")} />
+                        </div>
+                        <p>Neko</p>
+                    </div>
+                    <div className={cx("wrapper-addStory")}>
+                        <BigPlusIcon className={cx("icon-plus")} />
+                    </div>
+                </div>
+                <div className={cx("content")}>
+                    <div className={cx("navigation")}>
+                        <button className={cx("show-post", "active")}>
+                            <FontAwesomeIcon icon={faTableCells} className={cx("icon-post")} />
+                            <p>BÀI VIẾT</p>
+                        </button>
+                        <button className={cx("show-savedPost")}>
+                            <FontAwesomeIcon icon={faBookmark} className={cx("icon-savedPost")} />
+                            <p>ĐÃ LƯU</p>
+                        </button>
+                        <button className={cx("show-attackedTag")}>
+                            <FontAwesomeIcon icon={faClipboardUser} className={cx("icon-attackedTag")} />
+                            <p>ĐƯỢC GẮN THẺ</p>
+                        </button>
+                    </div>
+                    <div className={cx("list-posts")}>
+                        <img src="/img/DucThien.jpg" alt="" srcSet="" className={cx("post")} />
+                        <img src="/img/DucVan.jpg" alt="" srcSet="" className={cx("post")} />
+                        <img src="/img/DucThien.jpg" alt="" srcSet="" className={cx("post")} />
+                        <img src="/img/DucVan.jpg" alt="" srcSet="" className={cx("post")} />
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
